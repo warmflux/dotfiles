01151e4e54a5a940177d30875263dd3e0852d252
warmflux <oxetalenoy580@gmail.com>
2026-07-25 19:41:25 +0800

    modify some config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,15 +4,16 @@ end
 set fish_greeting ""
 starship init fish | source
 mise activate fish | source
-set -U fish_user_paths (go env GOPATH)/bin $fish_user_paths
+# set -U fish_user_paths (go env GOPATH)/bin $fish_user_paths
+
+set -gx EDITOR hx
+set -gx VISUAL hx
 
 function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	command yazi $argv --cwd-file="$tmp"
-	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
-		builtin cd -- "$cwd"
-	end
-	command rm -f -- "$tmp"
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    command yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+        builtin cd -- "$cwd"
+    end
+    command rm -f -- "$tmp"
 end
-
-
